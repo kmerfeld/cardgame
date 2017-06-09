1a68b0894a45f4ad7f87e7758e98854012eed85e
kyle merfeld <kmerfeld1@gmail.com>
2017-06-09 12:10:45 -0500

    clean up stuff

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-extern crate clap;
-use clap::{Arg, App, SubCommand};
-
 extern crate rand;
-mod Card;
-mod Deck;
-use Deck::create_deck;
+extern crate serde_json;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate clap;
 
-#[macro_use]
-extern crate serde_derive;
+mod card;
+mod deck;
+
+use clap::{Arg, App, SubCommand};
+use deck::create_deck;
+
+
 
 
 
 fn main() {
+    //clap options
     let matches = App::new("Cardgame")
         //.version(crate_version!())
         .author("Kyle Merfeld. <kmerfeld1@gmail.com>")
         .about("Tries to let you play a game")
         .subcommand(SubCommand::with_name("create")
+                    .version(crate_version!())
                     .about("Create deck")
                     .arg(Arg::with_name("deck_name")
                          .long("name")
@@ -29,13 +33,13 @@ fn main() {
                          .short("e")
                          .help("How much exp to grant to your cards")
                          .takes_value(true)
-                         .required(true))
+                         .default_value("2000"))
                     .arg(Arg::with_name("number_of_cards")
                          .long("cards")
                          .short("c")
                          .help("The number of cards in the deck")
                          .takes_value(true)
-                         .required(true)))
+                         .default_value("30")))
         .subcommand(SubCommand::with_name("server")
                     .about("Start a server (not working yet)"))
         .subcommand(SubCommand::with_name("client")
@@ -43,23 +47,27 @@ fn main() {
         .get_matches();
 
 
-
+    //handle the options provided
     match matches.subcommand() {
+        //When the create subcommand is run
         ("create", Some(create_matches)) =>{
             // Now we have a reference to create's matches
             println!("Creating a deck with {} cards", create_matches.value_of("number_of_cards").unwrap());
             println!("Creating a deck with {} exp ", create_matches.value_of("exp").unwrap());
             println!("Creating a deck named {}", create_matches.value_of("deck_name").unwrap());
+            //Create a deck with options
             create_deck(create_matches.value_of("number_of_cards").unwrap().parse::<i32>().unwrap(),
             create_matches.value_of("exp").unwrap().parse::<i32>().unwrap(),
             create_matches.value_of("deck_name").unwrap().to_owned());
         },
+        /*
         ("server", Some(server_matches)) => {
             println!("Not implemented yet");
         },
-        ("client", Some(server_matches)) => {
+        ("client", Some(client_matches)) => {
             println!("Not implemented yet");
         },
+        */
         ("", None)   => println!("Look at ./cardgame --help"),
         _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
     }
